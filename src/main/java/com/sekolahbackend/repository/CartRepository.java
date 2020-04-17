fe1fc8e696a77a385c96a37ebe4086ac6e8869d7
greenway926 <panji.cahyountoro@gmail.com>
2020-04-17 20:37:52 +0700

    add all files

---
+++ src/main/java/com/sekolahbackend/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package com.sekolahbackend.repository;
+
+import com.sekolahbackend.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+    Cart findByUserId(Integer userId);
+}
---
+++ src/main/java/com/sekolahbackend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sekolahbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sekolahbackend.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+}
